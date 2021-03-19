cf422729c3143fefada744c2c9af371a3414aaf0
Takayama Fumihiko <tekezo@pqrs.org>
2021-03-20 00:13:42 +0900

    Add ivars->keyboard,ivars->pointing to VirtualHIDDeviceRoot

--- src/DriverKit/Karabiner-DriverKit-VirtualHIDDevice/org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot.cpp
+++ src/DriverKit/Karabiner-DriverKit-VirtualHIDDevice/org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot.cpp
@@ -1,5 +1,7 @@
 #include "org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot.h"
 #include "IOBufferMemoryDescriptorUtility.hpp"
+#include "org_pqrs_Karabiner_DriverKit_VirtualHIDKeyboard.h"
+#include "org_pqrs_Karabiner_DriverKit_VirtualHIDPointing.h"
 #include "pqrs/karabiner/driverkit/virtual_hid_device_driver.hpp"
 #include "version.hpp"
 #include <HIDDriverKit/IOHIDDeviceKeys.h>
@@ -9,6 +11,9 @@
 #define LOG_PREFIX "Karabiner-DriverKit-VirtualHIDDeviceRoot " KARABINER_DRIVERKIT_VERSION
 
 struct org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot_IVars {
+  uint32_t keyboardCountryCode;
+  org_pqrs_Karabiner_DriverKit_VirtualHIDKeyboard* keyboard;
+  org_pqrs_Karabiner_DriverKit_VirtualHIDPointing* pointing;
 };
 
 bool org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot::init() {
@@ -28,6 +33,9 @@ bool org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot::init() {
 
 void org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot::free() {
   os_log(OS_LOG_DEFAULT, LOG_PREFIX " free");
+
+  OSSafeReleaseNULL(ivars->keyboard);
+  OSSafeReleaseNULL(ivars->pointing);
 
   IOSafeDeleteNULL(ivars, org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot_IVars, 1);
 
@@ -51,6 +59,13 @@ kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, Start) {
 
 kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, Stop) {
   os_log(OS_LOG_DEFAULT, LOG_PREFIX " Stop");
+
+  if (ivars->keyboard) {
+    ivars->keyboard->Stop(provider);
+  }
+  if (ivars->pointing) {
+    ivars->pointing->Stop(provider);
+  }
 
   return Stop(provider, SUPERDISPATCH);
 }
@@ -76,4 +91,46 @@ kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, NewUserCli
   }
 
   return kIOReturnSuccess;
+}
+
+kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, virtualHIDKeyboardInitialize) {
+  if (!ivars->keyboard) {
+    ivars->keyboardCountryCode = keyboardCountryCode;
+
+    IOService* client;
+
+    auto kr = Create(this, "VirtualHIDKeyboardProperties", &client);
+    if (kr != kIOReturnSuccess) {
+      os_log(OS_LOG_DEFAULT, LOG_PREFIX " IOService::Create failed: 0x%x", kr);
+      return kr;
+    }
+
+    ivars->keyboard = OSDynamicCast(org_pqrs_Karabiner_DriverKit_VirtualHIDKeyboard, client);
+    if (!ivars->keyboard) {
+      os_log(OS_LOG_DEFAULT, LOG_PREFIX " OSDynamicCast failed");
+      client->release();
+      return kIOReturnError;
+    }
+  }
+
+  return kIOReturnSuccess;
+}
+
+bool IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, virtualHIDKeyboardReady) {
+  return (ivars->keyboard && ivars->keyboard->getReady());
+}
+
+kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, virtualHIDKeyboardPostReport) {
+  if (ivars->keyboard) {
+    return ivars->keyboard->postReport(report);
+  }
+
+  return kIOReturnError;
+}
+
+kern_return_t IMPL(org_pqrs_Karabiner_DriverKit_VirtualHIDDeviceRoot, virtualHIDKeyboardReset) {
+  if (ivars->keyboard) {
+    return ivars->keyboard->reset();
+  }
+  return kIOReturnError;
 }
